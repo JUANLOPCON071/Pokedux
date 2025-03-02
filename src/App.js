79d7609a219efd3ed76701899f74e2473a5f9b8c
juanlopcon07 <juan.21futmas07@gmail.com>
2025-03-02 17:13:28 -0500

    Integrando Redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
 import { Col } from 'antd';
 import { getPokemon } from './api';
+import { setPokemons, setPokemons as setPokemonsActions } from './actions';
 import Searcher from './componentes/Searcher/Searcher';
 import PokemonList from './componentes/PokemonList/PokemonList';
 import logo from './statics/logo.svg'
 import './App.css';
 
-function App() {
-  const [pokemons, setPokemons] = useState([]);
+function App({ pokemons, setPokemons }) {
+  console.log('Pokemosn: ', pokemons);
+  
 
   useEffect(() => {
     const fetchPokemons = async () => {
@@ -31,4 +34,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsActions(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
